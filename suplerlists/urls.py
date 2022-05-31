016b25ab6666916652993348104b7884f7f7a2c6
tomato <1564338910@bupt.edu.cn>
2022-05-31 09:11:00 +0800

    new URL, view and template to display lists

--- suplerlists/urls.py
+++ suplerlists/urls.py
@@ -17,5 +17,6 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('',views.home_page, name='home')        
+    path('',views.home_page, name='home'), 
+    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list')
 ]
