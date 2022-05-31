94456e3092b0df5600aed6b11281ce45e75b28f0
tomato <1564338910@bupt.edu.cn>
2022-05-31 12:39:29 +0800

    ADD a url for creating a new list via POST

--- lists/tests.py
+++ lists/tests.py
@@ -1,14 +1,22 @@
 from django.test import TestCase
-from lists.models import Item
-class ItemModelTest(TestCase):
+from lists.models import Item, List
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -16,21 +24,25 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(first_saved_item.list, list_)
+
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-class LiveViewTest(TestCase):
+class ListViewTest(TestCase):
 
-    def test_uses_list_temp(self):
+    def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
-        self.assertTemplateUsed(response, 'lists.html')
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -44,11 +56,6 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'lists.html', {'items':items})
+    return render(request, 'list.html', {'items':items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
