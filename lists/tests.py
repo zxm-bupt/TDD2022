5fa0ec25b065eab66a9a23203a132bc26fe8f6c3
tomato <1564338910@bupt.edu.cn>
2022-05-31 21:16:37 +0800

    new URL + view for adding to existing lists. FT passes:-)

--- lists/tests.py
+++ lists/tests.py
@@ -66,5 +66,34 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data = {'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
-        
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
+
+class NewItemTest(TestCase):
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+                f'/lists/{correct_list.id}/add_item',
+                data={'item_text': 'A new item for an existing list'}
+                )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+                f'/lists/{correct_list.id}/add_item',
+                data={'item_text': 'A new item for an existing list'}
+                )
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
 # Create your tests here
--- suplerlists/urls.py
+++ suplerlists/urls.py
@@ -19,5 +19,6 @@ from lists import views
 urlpatterns = [
     path('',views.home_page, name='home'), 
     path('lists/new', views.new_list, name='new_list'),
-    re_path(r'^lists/(.+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item')
 ]
